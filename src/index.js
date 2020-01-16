b33cb41dc3f9db1a441e103a7b58920ef8af51be
Lisa Hillebrand <lisa.hillebrand@posteo.de>
2020-01-16 14:45:15 +0100

    Remove redux

--- src/index.js
+++ src/index.js
@@ -3,9 +3,7 @@ import ReactDOM from 'react-dom';
 import { ThemeProvider } from '@material-ui/core';
 import { BrowserRouter } from 'react-router-dom';
 import './index.css';
-import { Provider } from 'react-redux';
 import App from './App';
-import store from './state/store';
 
 import Theme from './layout/Theme';
 import ScrollToTop from './components/common/ScrollToTop';
@@ -13,13 +11,11 @@ import ScrollToTop from './components/common/ScrollToTop';
 const rootElement = document.getElementById('root');
 
 ReactDOM.render(
-    <Provider store={store}>
-        <ThemeProvider theme={Theme}>
-            <BrowserRouter>
-                <ScrollToTop />
-                <App />
-            </BrowserRouter>
-        </ThemeProvider>
-    </Provider>,
+    <ThemeProvider theme={Theme}>
+        <BrowserRouter>
+            <ScrollToTop />
+            <App />
+        </BrowserRouter>
+    </ThemeProvider>,
     rootElement
 );
--- src/mocks/mockStore.js
+++
@@ -1,6 +0,0 @@
-import thunk from 'redux-thunk';
-import configureMockStore from 'redux-mock-store';
-
-const mockStore = configureMockStore([thunk]);
-
-export default mockStore;
