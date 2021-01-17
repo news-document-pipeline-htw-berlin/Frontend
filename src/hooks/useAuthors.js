a907d4cd050557229e8e1a8da8b624cbc61bade2
katharinakoslowski <s0561010@htw-berlin.de>
2021-01-17 18:22:11 +0100

    added hook for fetching author data

---
+++ src/hooks/useAuthors.js
@@ -0,0 +1,26 @@
+import { useState, useEffect } from 'react';
+import { httpInstance } from '../state/httpInstance';
+import stringToJSON from '../components/common/stringToJSON';
+
+function useAuthor(id) {
+    const [author, setAuthor] = useState({});
+
+    useEffect(() => {
+        httpInstance
+            .get('/authors', {
+                params: {
+                    id
+                }
+            })
+            .then(response => {
+                setAuthor(stringToJSON(response.data));
+            })
+            .catch(error => {
+                setAuthor(null);
+            });
+    }, [id]);
+
+    return [author, setAuthor];
+}
+
+export { useAuthor };
